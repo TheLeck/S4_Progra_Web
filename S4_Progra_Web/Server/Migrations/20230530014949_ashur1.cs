c3ff4b7d968b8949224db8bbc52bf3edfaf436c0
TheLeck <survigam@gmail.com>
2023-05-29 20:41:18 -0600

    pues ese ese ultimo eso

---
+++ S4_Progra_Web/Server/Migrations/20230530014949_ashur1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S4_Progra_Web.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ashur1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
